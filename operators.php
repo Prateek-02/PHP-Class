449bba739cdd2d328fb5f914353aa9243198d536
Prateek <Prateekrajgrd74@gmail.com>
2024-08-23 16:54:33 +0530

    operators.php

--- operators.php
+++ operators.php
@@ -1,14 +1,16 @@
 <?php
+/*
     $a = 10;
     $b = 9;
     echo $a+$b;
     $a++;
     echo " ",$a;
+*/
 ?>
 
 
 <?php
-
+/*
     #1
     $a = readline("Enter a: ");
     $b = readline("Enter b: ");
@@ -34,15 +36,52 @@
     $a--;
     echo "After decremnting a: ",$a,"\n";
 
-    // #4
+    #4
     $x = $y = $z = 9;
     echo $x," ",$y," ",$z;
+*/
 ?>
 
+
+
+
+
 <?php 
+/*
+#5
     $username = readline("Enter username: ");
 
     $greeting = !empty($username) ? "Welcome, $username" : "Welcome, Guest";
 
     echo $greeting;
+*/
 ?>
+
+<?php
+#6
+
+    $nationality = readline("Enter your nationality: ");
+    $age = readline("Enter your age: ");
+    echo $nationality == "Indian" ? ($age >= 18 ? "Eligibe" : "Not eligible") : "Not eligible";
+?>
+
+<?php
+#7
+    $marks = readline("Enter your marks: ");
+    if($marks>=90){
+        echo "A";
+    }
+    else if($marks>=80 && $marks<90){
+        echo "B";
+    }
+    else if($marks>=70 && $marks < 80){
+        echo "C";
+    }
+    else if($marks>=60 && $marks < 70){
+        echo "D";
+    }
+    else{
+        echo "F";
+    }
+?>
+
