8509311398d63033c5f0a1057c3929f76962872c
Prateek <Prateekrajgrd74@gmail.com>
2024-08-28 11:02:17 +0530

    controlStructures.c++

--- controlStructures.php
+++ controlStructures.php
@@ -152,36 +152,99 @@
 <!-- 3 -->
 
 <?php
-    $income = readline("Enter your income: ");
-    $age = readline("Enter your age: ");
-    $tax;
+    // $income = readline("Enter your income: ");
+    // $age = readline("Enter your age: ");
+    // $tax;
 
-    if($income < 10000){
-        echo "No tax applied";
-        $tax = 0;
+    // if($income < 10000){
+    //     echo "No tax applied";
+    //     $tax = 0;
+    // }
+
+    // else if($income >= 10000 && $income <=20000){
+    //     if($age<65){
+    //         echo "10% tax is applied\n";
+    //         $tax = $income*(0.10);
+    //     }
+    //     else{
+    //         echo "5% tax is applied\n";
+    //         $tax = $income*(0.05);
+    //     }
+    // }
+    // else if($income > 20000){
+    //     if($age<65){
+    //         echo "20% tax is applied\n";
+    //         $tax = $income*(0.20);
+    //     }
+    //     else{
+    //         echo "15% tax is applied\n";
+    //         $tax = $income*(0.15);
+    //     }
+    // }
+
+    // echo "Tax to be paid is: ",$tax;
+
+?>
+
+
+<!-- 4 -->
+
+<?php
+    // $mealType  = readline("Enter your meal type: ");
+
+    // switch($mealType){
+    //     case "Breakfast":
+    //         echo "Select any food Item: \n";
+    //         echo "1. Paratha";
+    //         echo "2. Idli";
+    //         echo "3. Sandwich";
+    //         echo "4. Cereal";
+    //         switch($foodItem){
+    //             case 1:
+    //         }
+    //         break;
+    //     case "Lunch":
+    //         "Select any food Item: \n";
+    //         echo "1. Dal Rice";
+    //         echo "2. Rajah Rice";
+    //         echo "3. Thali";
+    //         echo "4. Fired Rice";
+    //         break;
+    //     case "Dinner":
+    //         echo "Select any food Item: \n";
+    //         echo "1. abcd";
+    //         echo "2. efgh";
+    //         echo "3. ijkl";
+    //         echo "4. mnop";
+    //         switch($foodItem){
+
+    //         }
+    //         break;
+        
+    //     case "Snack":
+    //         echo "Select any food Item: \n";
+    //         echo "1. Chai pakoda";
+    //         echo "2. Noodles";
+    //         echo "3. Pav Bhaji";
+    //         echo "4. Poha";
+    //         $foodItem = intval(readline());
+    //         switch($foodItem){
+
+    //         }
+    // }
+?>
+
+
+<!-- 5 -->
+
+<?php
+    $type = readline("Residential or Commercial: ");
+    $utility = readline("Utility: ");
+    $bill;
+
+    if($type == "Residential"){
+        if($utiltiy >=0 && $utility <=100){
+            $bill = $utility * 50.5;
+        }
     }
-
-    else if($income >= 10000 && $income <=20000){
-        if($age<65){
-            echo "10% tax is applied\n";
-            $tax = $income*(0.10);
-        }
-        else{
-            echo "5% tax is applied\n";
-            $tax = $income*(0.05);
-        }
-    }
-    else if($income > 20000){
-        if($age<65){
-            echo "20% tax is applied\n";
-            $tax = $income*(0.20);
-        }
-        else{
-            echo "15% tax is applied\n";
-            $tax = $income*(0.15);
-        }
-    }
-
-    echo "Tax to be paid is: ",$tax;
-
 ?>
