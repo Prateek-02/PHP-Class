98daae505358479cff47857a480dcad614b83383
Prateek <Prateekrajgrd74@gmail.com>
2024-08-30 15:39:33 +0530

    loops.php

--- loops.php
+++ loops.php
@@ -1,12 +1,23 @@
 <?php
-$sum = 0;
-$n = 1;
-do{
-    $sum+= $n;
-    $n++;
-}
-while($n<=10);
+    $sum = 0;
+    $n = readline("Enter number: ");
+    $i=1;
+    do{
+        $sum+= $i;
+        $i++;
+    }
+    while($i<=$n);
 
-echo "The sum is: ",$sum;
+    echo "The sum is: ",$sum;
 
+?>
+
+<?php
+    $n = 5;
+    for($i=1; $i <= $n;$i++){
+        for($j = 1;$j <= $i; $j++){
+            echo $i;
+        }
+        echo "\n";
+    }
 ?>
