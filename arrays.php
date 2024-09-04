6d59fc1e6c72b9fb125d81649c14d7221eba5136
Prateek <Prateekrajgrd74@gmail.com>
2024-09-04 09:47:43 +0530

    arrays.php

--- arrays.php
+++ arrays.php
@@ -16,6 +16,22 @@
 <?php
     $color = array("c1" => "red", "c2" => "green", "c3" => "blue");
     foreach ($color as $key => $value) {
-        echo $key, " => ", $value," ";
+        echo $key, " => ", $value,", ";
     }
+?>
+
+
+<?php
+    $color = array('white','green','red','blue','black');
+
+    $cnt = count($color);
+
+    echo "<ul>";
+
+    for($i=0;$i<$cnt;$i++){
+        echo "<li>", $color[$i];
+    }
+   
+
+
 ?>
