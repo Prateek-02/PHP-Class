21c4e81f979c1575c4960d245b1159ccc36d4835
Prateek <Prateekrajgrd74@gmail.com>
2024-09-11 09:34:20 +0530

    multiarr.php

--- multidimentionalArray.php
+++ multidimentionalArray.php
@@ -4,13 +4,16 @@
         array('Aditya',8.5,'pass'),
         array('Rahul',4.9,'fail')
     );
-
+    echo "<table border = '2px'>";
     foreach($result as $res){
+        echo "<tr>";
         foreach($res as $r){
-            echo $r," ";
+            echo "<td> $r </td>";
         }
         echo"<br>";
+        echo "</tr>";
     }
+    echo "</table>";
     echo "<pre>";
     print_r($result);
    
