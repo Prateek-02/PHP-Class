cb9bab57eed6f04f13ffb23d98ad3df41b9fcea1
Prateek <Prateekrajgrd74@gmail.com>
2024-09-16 16:41:22 +0530

    funcScope.php

--- functionScope.php
+++ functionScope.php
@@ -1,10 +1,26 @@
 <?php
+    // function sum(){
+    //     $s = $GLOBALS['a'] + $GLOBALS['b'];
+    //     return $s;
+    // }
+
+    // $a = 10;
+    // $b = 20;
+    // echo sum();
+?>
+
+<?php
     function sum(){
-        $s = $GLOBALS['a'] + $GLOBALS['b'];
-        return $s;
+        static $a = 10;
+        $b = 20;
+
+        $a++;
+        $b++;
+
+        return $a." ".$b;
+
     }
 
-    $a = 10;
-    $b = 20;
     echo sum();
+
 ?>
