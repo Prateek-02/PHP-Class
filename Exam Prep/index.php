de3f58981671cdc3ab4d5d5e1618c40795ee5773
Prateek <Prateekrajgrd74@gmail.com>
2024-11-28 00:25:43 +0530

    a.php

---
+++ Exam Prep/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        This is my first php website
+        <?php
+            echo "Hello world and this is printed using php";
+            echo "<br>";
+            
+
+            $var1 = 34;
+            $var2 = 12;
+
+            echo $var1+$var2."<br>";
+
+            echo $var1 - $var2;
+        ?>
+    </div>
+</body>
+</html>
